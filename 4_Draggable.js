7ca27ff2dbb34a3bbaf8d687934ee4c2803d62d2
eportocarrero94 <ewer_rpv@hotmail.com>
2022-10-25 18:06:50 -0500

    25 de octubre

--- 4_Draggable.js
+++ 4_Draggable.js
@@ -1,15 +1,18 @@
-const container = document.querySelectorAll(`main`)
+/* Ejemplo con Draggable */
 
-const draggable = new Draggable.default(container, {
+const container1 = document.getElementById(`main1`)
+
+const draggable = new Draggable.default(container1, {
     draggable:"div"
 })
 
 draggable.on(`drag:start`, () => console.log(`Comienza el drag`))
 draggable.on(`drag:stop`, () => console.log(`Termina el drag`))
 
+/* Ejemplo con Droppable */
 
-const containers2 = document.querySelectorAll(`.container`)
-const droppable = new Droppable.default(containers2, {
+const container2 = document.querySelectorAll(`.container`)
+const droppable = new Droppable.default(container2, {
     draggable: '.item',
     dropzone: '.dropzone'
 })
@@ -17,3 +20,11 @@ const droppable = new Droppable.default(containers2, {
   
   droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
   droppable.on('droppable:returned', () => console.log('droppable:returned'));
+
+/* Ejemplo con Sortable */
+
+  const container3 = document.getElementById(`main2`)
+
+  const sortable = new Sortable.default(container3, {
+    draggable:"div"
+})
