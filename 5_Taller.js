90b7dd4f96bab3c80a418ada6f04e2eed38c28b1
eportocarrero94 <ewer_rpv@hotmail.com>
2022-10-27 18:16:01 -0500

    27 de octubre 3 xd

--- 5_Taller.js
+++ 5_Taller.js
@@ -14,25 +14,43 @@ function fillTasks(data) {
 
         let taskTitle = document.createElement("h3")
         taskTitle.innerHTML = d.title
+    
+        // let taskState = document.createElement(`p`)
+        // taskState.innerHTML = d.state
+
+        let taskPerson = document.createElement(`p`)
+        taskPerson.innerHTML=`Responsable: <span>${d.person}</span>`
+    
+        // let taskPerson = document.createElement(`span`)
+        // personP.innerHTML=`Responsable: `
+        // taskPerson.innerHTML = d.person
+        // personP.appendChild(taskPerson)
+        // newTask.appendChild(personP)
+        
+        let taskDeadline = document.createElement(`p`)
+        taskDeadline.innerHTML=`Plazo: <span>${d.deadline}</span>`
+
+
         newTask.appendChild(taskTitle)
-
-        let taskState = document.createElement(`p`)
-        taskState.innerHTML = d.state
-        newTask.appendChild(taskState)
-
-        let taskPerson = document.createElement()
-
-        let plazoP = document.createElement(`p`)
-        let deadline = document.createElement(`span`)
-        plazoP.innerHTML = `Plazo: `
-        deadline.innerHTML = d.deadline
-        plazoP.appendChild(deadline)
-        newTask.appendChild(plazoP)
+        // newTask.appendChild(taskState)
+        newTask.appendChild(taskPerson)
+        newTask.appendChild(taskDeadline)
         
         
 
-        let column = document.getElementById(`doneTasks`)
-        column.appendChild(newTask)
+        let columnToDo = document.getElementById(`todoTask`)
+        let columnInProgress = document.getElementById(`inProgressTask`)
+        let columnDone = document.getElementById(`doneTask`)
+
+        if (d.state === "to-do"){
+            columnToDo.appendChild(newTask)
+        }
+        if (d.state === "in-progress"){
+            columnInProgress.appendChild(newTask)
+        }
+        if (d.state === "done"){
+            columnDone.appendChild(newTask)
+        }
     })
 }
 
