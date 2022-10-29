b617e2fcf7fb2ac6b6debf2e9b7a1fe45d0229f6
eportocarrero94 <ewer_rpv@hotmail.com>
2022-10-29 12:32:50 -0500

    29 de octubre 1

--- 5_Taller.js
+++ 5_Taller.js
@@ -1,16 +1,60 @@
 const API_URL = "https://my-json-server.typicode.com/eportocarrero94/Librerias_js/tasks"
 
+/* Función para formatear fecha */ 
+function unixToDate(time, format = `DD/MM/YYYY`) {
+    return moment.unix(time).format(format)
+}
 
+
+/* Agrega animación de ingreso al formulario */
+let boton = document.getElementById(`newTask`)
+let formulario = document.getElementById(`formElement`)
+let botonCancelar = document.getElementById(`cancelForm`)
+
+boton.addEventListener(`click`, () => {
+    formulario.classList.add(`active`)
+})
+
+botonCancelar.addEventListener(`click`, () => {
+    formulario.classList.remove(`active`)
+})
+
+
+/* Agregar funciones Touch */
+delete Hammer.defaults.cssProps.userSelect
+
+const body = document.getElementById(`body`)
+const hammerjs = new Hammer(body)
+hammerjs.on("panleft panright ", ev => {
+    if (ev.pointerType === `touch`) {
+        if (ev.type === `panleft` && ev.distance > 100) {
+            formulario.classList.add(`active`)
+            }
+        if (ev.type === `panright` && ev.distance > 100) {
+                formulario.classList.remove(`active`)
+            }
+    }
+})
+
+
+/*  */
+const sortable = new Sortable.default(document.querySelectorAll(`.task-container`), {
+    draggable: `article`
+})
+
+
+/* Petición de una API */
 axios.get(API_URL)
 
     .then(respuesta => {
-        console.log(respuesta)
+        // console.log(respuesta)
         return fillTasks(respuesta.data)})
     .catch(error => console.log(error))
-
+ 
 function fillTasks(data) {
     data.map( d=> {
         let newTask = document.createElement("article")
+        newTask.classList.add(`task`)
 
         let taskTitle = document.createElement("h3")
         taskTitle.innerHTML = d.title
@@ -28,7 +72,7 @@ function fillTasks(data) {
         // newTask.appendChild(personP)
         
         let taskDeadline = document.createElement(`p`)
-        taskDeadline.innerHTML=`Plazo: <span>${d.deadline}</span>`
+        taskDeadline.innerHTML=`Plazo: <span>${unixToDate(d.deadline)}</span>`
 
 
         newTask.appendChild(taskTitle)
@@ -38,9 +82,9 @@ function fillTasks(data) {
         
         
 
-        let columnToDo = document.getElementById(`todoTask`)
-        let columnInProgress = document.getElementById(`inProgressTask`)
-        let columnDone = document.getElementById(`doneTask`)
+        let columnToDo = document.getElementById(`todoTasks`)
+        let columnInProgress = document.getElementById(`inProgressTasks`)
+        let columnDone = document.getElementById(`doneTasks`)
 
         if (d.state === "to-do"){
             columnToDo.appendChild(newTask)
@@ -53,4 +97,5 @@ function fillTasks(data) {
         }
     })
 }
+
 
